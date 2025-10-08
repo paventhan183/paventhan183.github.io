c1ad5ad5d8a8baed711c441cce8bb0c028869542
paventhan <paventhanbtech1990@gmail.com>
2025-10-08 17:51:27 +0530

    changes added

--- index.js
+++ index.js
@@ -141,3 +141,24 @@ function highlightNavOnScroll() {
 
 window.addEventListener('scroll', highlightNavOnScroll);
 document.addEventListener('DOMContentLoaded', highlightNavOnScroll); // Run on initial load to highlight the top link
+
+// Slideshow Logic
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("before-after-slide");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  
+  slides[slideIndex-1].style.display = "flex";
+}
